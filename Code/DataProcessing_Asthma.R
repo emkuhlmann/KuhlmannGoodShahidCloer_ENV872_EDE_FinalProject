61c295a9d4e535439aa52b6b6fc69acd9d0a7292
emkuhlmann <emily.kuhlmann@duke.edu>
2023-11-09 16:36:55 -0500

    Asthma data exploration

--- Code/DataProcessing_Asthma.R
+++ Code/DataProcessing_Asthma.R
@@ -1,9 +1,12 @@
 library(here)
 library(tidyverse)
-#install.packages("naniar")
-library(naniar)
 #importing dataset and replacing "suppressed" entries in the values column as NAs
 CA_Asthma_EDVisits <- read.csv(here("Data/Raw/CA_Asthma_EDVisits.csv"), 
                                stringsAsFactors = T, 
                                na.strings = c("Suppressed"))
+colnames(CA_Asthma_EDVisits)
+summary(CA_Asthma_EDVisits) #14 NAs in the value column, coming from Sierra and Alpine counties
+summary(CA_Asthma_EDVisits$County) #15 values per county, one per year
 
+
+         
