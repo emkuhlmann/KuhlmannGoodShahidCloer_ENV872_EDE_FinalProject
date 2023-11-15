00917e913942d91385c20f8e3caebc62b4d27946
emkuhlmann <emily.kuhlmann@duke.edu>
2023-11-15 18:19:20 -0500

    Exploring pulling population data

---
+++ Code/PopulationData.R
@@ -0,0 +1,14 @@
+#install.packages("tidycensus")
+library(tidycensus)
+
+census_api_key("9821d76d58cfa47ae353a1c89a8f3f4cc706b9a0")
+install = TRUE
+
+variables <- load_variables(2015, "acs1")
+#total population <-	B01003_001
+
+pop_data <- get_acs(geography = "county",
+                    variables = c(total_pop = "B01003_001"),
+                    state = "CA",
+                    year = 2005,
+                    survey = "acs1")
