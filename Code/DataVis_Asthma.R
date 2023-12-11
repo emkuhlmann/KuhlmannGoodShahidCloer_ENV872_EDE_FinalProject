d42b570660076309299f977e0deac40052d38858
emkuhlmann <emily.kuhlmann@duke.edu>
2023-12-11 12:37:01 -0500

    Changing heatmap color ramp so that it is color blind accessible

--- Code/DataVis_Asthma.R
+++ Code/DataVis_Asthma.R
@@ -10,9 +10,9 @@ AsthmaVisits_heatmap <- ggplot(Asthma_data_processed,
                                aes(x = Year, y = County, fill = visits_per100k)) +
   geom_tile() +
   scale_fill_distiller(name = "ED Visits for Asthma per 100k",
-                       palette = "RdYlGn",
+                       palette = "YlOrBr",
                        breaks = c(100, 300, 500, 700),
-                       direction = -1) +
+                       direction = 1) +
   theme(legend.title = element_text(size = 10),
         legend.text = element_text(size = 8),
         legend.key.size = unit(1, "lines"))
